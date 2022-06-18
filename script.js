fbaf62d795e9a2490c7f0613b05b0fda635aab07
Alina Bregvadze <82210320+alinbrg@users.noreply.github.com>
2022-06-18 22:20:45 +0400

    javascript variables, primitive data types

---
+++ script.js
@@ -0,0 +1,100 @@
+var userAge = 20;
+userSurname = "Example";
+
+let userName = "Nino";
+let userEmail, userInfo, userCity;
+// console.log(typeof userEmail);
+const birthYear = 2000;
+
+userName = "Giorgi";
+userEmail = "example@gmail.com";
+
+// console.log(typeof userEmail);
+userEmail = undefined;
+// console.log(typeof userEmail);
+
+// let hTag =  document.querySelector("h1");
+// hTag.innerText = userName;
+
+let userName1 = "Tea",
+	userAge1 = 20,
+	myBoolTrue = true,
+	myBoolFalse = false,
+	myUndefined = undefined,
+	myNull = null;
+
+/*
+console.log(typeof userName1, "====typeof userName1");
+console.log(typeof userAge1, "====typeof userAge1");
+console.log(typeof myBoolTrue, "====typeof myBoolTrue");
+console.log(typeof myBoolFalse, "====typeof myBoolFalse");
+console.log(typeof myUndefined, "====typeof myUndefined");
+console.log(typeof myNull, "====typeof myNull");
+*/
+
+const myNum1 = 5,
+	myNum2 = 2,
+	myNum0 = 0;
+
+let addintion = myNum1 + myNum2;
+let addintionMult = (myNum1 + myNum2) * myNum2;
+// console.log("addintion", addintion);
+
+let substraction = myNum1 - myNum2;
+// console.log("substraction", substraction);
+
+let multiplication = myNum1 * myNum2;
+// console.log("multiplication", multiplication);
+
+let division = myNum1 / myNum2;
+// console.log("division", division);
+
+let division0 = myNum1 / myNum0;
+// console.log("division 0", division0);
+
+let modulo = myNum1 % myNum2;
+// console.log("modulo, ნაშთი", modulo);
+
+let power = myNum1 ** myNum2;
+// console.log("power", power);
+
+let myNum3 = "5";
+// myNum3 = "Apple5";
+let myNum4 = "2";
+
+let addintion2 = myNum3 + myNum4;
+// console.log("addintion", addintion2);
+// NaN not a Number
+
+let substraction2 = myNum3 - myNum4;
+// console.log("substraction", substraction2);
+
+let multiplication2 = myNum3 * myNum4;
+// console.log("multiplication", multiplication2);
+
+let division2 = myNum3 / myNum4;
+// console.log("division", division2);
+
+let modulo2 = myNum3 % myNum4;
+// console.log("modulo, ნაშთი", modulo2);
+
+let power2 = myNum3 ** myNum4;
+// console.log("power", power2);
+
+let myNum5 = "Apple5";
+let myNum6 = 5;
+let myNum7 = "5";
+
+// console.log(myNum5 + myNum5 + myNum6);
+// console.log(myNum6 + myNum6 + myNum5);
+// console.log(myNum6 + myNum6 + myNum7);
+// console.log(myNum7 + myNum6 + myNum6);
+
+let myText1 = "example text " + myNum5 + " another example text " + myNum7;
+let myText2 = `example text ${myNum5} another example text ${myNum6 + 10}`;
+
+// console.log(myText1);
+// console.error(myText2);
+
+// alert(myText2);
+// prompt("how old are you?");
